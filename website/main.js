64fa3889942515d51b480b695e86c87318729fb5
jung27 <91370082+jung27@users.noreply.github.com>
2022-03-13 20:39:22 +0900

    Update main.js

--- website/main.js
+++ website/main.js
@@ -109,6 +109,7 @@ function update() {
       ctx.font = "70px Fira";
       ctx.fillStyle = "lime";
       ctx.fillText("press space key to replay", 900, 775);
+      ctx.stroke();
       over = true;
       arrows.length = 0;
       balloon.x = 100;
