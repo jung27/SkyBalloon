8209b2ecfff1b6627d9c2e6657d6a8a8eb3eb6f3
jung27 <91370082+jung27@users.noreply.github.com>
2022-03-18 22:18:11 +0900

    Update main.js

--- website/main.js
+++ website/main.js
@@ -249,7 +249,7 @@ function update() {
         });
         cool.set("arrow", [0, 0, 100, Arrow]);
         cool.set("follow", [2100, 0, 500, Follow]);
-        cool.set("wind", [0, 0, 1750, Wind]);
+        cool.set("wind", [0, 0, 1400, Wind]);
         pjs.forEach((v) => {
           v.length = 0;
         });
